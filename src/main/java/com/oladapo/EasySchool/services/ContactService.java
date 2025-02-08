798558c18c3a829844b6720e778b7796d6d96235
oladapo <oladapoelijah49@gmail.com>
2025-02-08 18:47:25 +0100

    request, session, application scope

--- src/main/java/com/oladapo/EasySchool/services/ContactService.java
+++ src/main/java/com/oladapo/EasySchool/services/ContactService.java
@@ -3,15 +3,35 @@ package com.oladapo.EasySchool.services;
 import com.oladapo.EasySchool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
+//@RequestScope
+//@SessionScope
+@ApplicationScope
 public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService(){
+        System.out.println("Contact Service Bean Initialized");
+    }
 
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
 
         log.info(contact.toString());
         return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
